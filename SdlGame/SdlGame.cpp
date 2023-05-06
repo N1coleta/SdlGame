729c586e1b8ff36d72f8306de5a854ede12689c5
Nicoleta Iancu <nicoleta.v.iancu@gmail.com>
2023-05-06 22:09:40 +0300

    cats vs hoomans game

--- SdlGame/SdlGame.cpp
+++ SdlGame/SdlGame.cpp
@@ -1,26 +1,50 @@
-#include <random>
-#include <iostream>
 #include <ctime>
+#include <iostream>
+#include <random>
 using namespace std;
 int main()
 {
-	default_random_engine randomGen(time(NULL));
-	uniform_real_distribution<float>attackroll(0.0f, 1.0f);
-	float attack;
-	int snakeHealth = 100;
-	int cnt = 0;
-	while (snakeHealth > 0) {
-		attack = attackroll(randomGen);
-		if (attack <= 0.3f) {
-			cout << "The snake will fall to the legendary cat!!! :)\n";
-			snakeHealth -= 10;
-			cnt++;
-		}
-		else {
-			cout << "NOOOOOOOO. U missed. :(\n";
-			cnt++;
-		}
-	}
-	cout << cnt;
-
+    default_random_engine randomGen(time(0));
+    uniform_int_distribution<int>attackrollcat(0, 10);
+    uniform_int_distribution<int>attackrollhooman(0, 11);
+    cout << "====NOW THE EPIC BATLE BETWEN HOOMANS AND CATS SHALL BEGIN===== \n";
+    int nrhoomans, nrcats;
+    cout << "The hooman input is  ";
+    cin >> nrhoomans;
+    cout << "The cat input is ";
+    cin >> nrcats;
+    int healthcat = 900;
+    int healthhooman = 100;
+    int copie1 = healthcat;
+    int copie2 = healthhooman;
+    int attack1, attack2;
+    int deadcat = 0;
+    int deadhooman = 0;
+    cout << "=========battle noises intensify =========\n \n";
+    while (nrcats > 0 && nrhoomans > 0) {
+        attack1 = attackrollcat(randomGen);
+        healthhooman -= attack1;
+        attack2 = attackrollhooman(randomGen);
+        healthcat -= attack2;
+        if (healthcat <= 0) {
+            nrcats--;
+            healthcat = copie1;
+            deadcat++;
+        }
+        if (healthhooman <= 0) {
+            nrhoomans--;
+            healthhooman = copie2;
+            deadhooman++;
+        }
+    }
+    if (nrhoomans <= 0 && nrcats <= 0) {
+        cout << "draw( i wish the cats would have won ) :( \n \n";
+    }
+    else if (nrhoomans <= 0) {
+        cout << "CAT WON YAAAAAAAAAAAAAAY \n \n";
+    }
+    else {
+        cout << "hoomans won :( \n \n";
+    }
+    cout << deadhooman << " hoomans died and " << deadcat << " loaf of bread have died \n";
 }
