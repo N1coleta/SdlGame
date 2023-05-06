349c277c33903bbe7e985bc9851f1df2e5a1b0b2
Nicoleta Iancu <nicoleta.v.iancu@gmail.com>
2023-05-06 21:22:41 +0300

    joc1

--- SdlGame/SdlGame.cpp
+++ SdlGame/SdlGame.cpp
@@ -1,7 +1,26 @@
-
+#include <random>
 #include <iostream>
+#include <ctime>
 using namespace std;
 int main()
 {
-    cout << "Hello World!\n";
+	default_random_engine randomGen(time(NULL));
+	uniform_real_distribution<float>attackroll(0.0f, 1.0f);
+	float attack;
+	int snakeHealth = 100;
+	int cnt = 0;
+	while (snakeHealth > 0) {
+		attack = attackroll(randomGen);
+		if (attack <= 0.3f) {
+			cout << "The snake will fall to the legendary cat!!! :)\n";
+			snakeHealth -= 10;
+			cnt++;
+		}
+		else {
+			cout << "NOOOOOOOO. U missed. :(\n";
+			cnt++;
+		}
+	}
+	cout << cnt;
+
 }
